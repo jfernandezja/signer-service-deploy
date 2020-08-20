445fa5153958449977c3470c990f8427691c17e0
Juan Carlos Fernández Jara <jfernandezja@uoc.edu>
2020-08-20 18:52:53 +0200

    production deploy

---
+++ services/patch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker build -t juancarlosfernandez/oauth-service:master services/oauth-service
+docker build -t juancarlosfernandez/signer-service:master services/signer-service
+
+docker push juancarlosfernandez/oauth-service:master
+docker push juancarlosfernandez/signer-service:master
+
+#kubectl patch deployment oauth-service -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
+#kubectl patch deployment signer-service -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
