d26c50006f62ae2446dd89183a8b1c325b57cf8f
Juan Carlos Fernández Jara <jfernandezja@uoc.edu>
2020-08-21 19:12:17 +0200

    deployment

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-eksctl create cluster -f eks-service.yaml
-kubectl apply -f ../kubernetes/deployment.yaml
-kubectl apply -f ../kubernetes/service.yaml
+eksctl create cluster -f eks/eks-service.yaml
+kubectl apply -f kubernetes/deployment.yaml
+kubectl apply -f kubernetes/service.yaml
 kubectl get services
